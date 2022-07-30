3b20bcfd2e4c052f8ce9e4af65e41f1515dff0e1
hoalam0805 <hoalam33.k43@gmail.com>
2022-07-30 09:59:09 +0700

    Add responsive css for mobile tablet device

--- src/App.js
+++ src/App.js
@@ -6,26 +6,15 @@ import Filters from './components/Filters';
 const { Title } = Typography;
 
 function App() {
-  return (
-    <div
-      style={{
-        width: 500,
-        margin: '0 auto',
-        display: 'flex',
-        flexDirection: 'column',
-        backgroundColor: 'white',
-        padding: 20,
-        boxShadow: "rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px",
-        borderRadius: 5,
-        height: '90vh',
-      }}
-    >
-      <Title style={{ textAlign: 'center' }}>MINI TODOS</Title>
-      <Filters />
-      <Divider />
-      <TodoList />
-    </div>
-  );
+
+    return (
+        <div className="app-container">
+            <Title style={{ textAlign: 'center' }}>MINI TODOS</Title>
+            <Filters />
+            <Divider />
+            <TodoList />
+        </div>
+    );
 }
 
 export default App;
